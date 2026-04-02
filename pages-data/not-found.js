9d8407231a7eedde1191191322d5ff0b2016db01
Artem Safin <artemsafin.webdev@gmail.com>
2026-04-02 20:24:08 +0300

    По странице 404

---
+++ pages-data/not-found.js
@@ -0,0 +1,11 @@
+export default {
+  "/404.html": {
+    title: "Страница не найдена",
+    error404: {
+      title: "Извините, страница не найдена",
+      text: "Вы можете вернуться на",
+      homeLinkText: "главную",
+      homeLinkHref: "/",
+    },
+  },
+};
