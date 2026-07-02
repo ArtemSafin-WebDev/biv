7de2a33d4b4f59662c39e40d9b0ab7ba4200e3bb
Artem Safin <artemsafin.webdev@gmail.com>
2026-07-02 03:07:56 +0300

    Каталог новостей

---
+++ pages-data/news.js
@@ -0,0 +1,92 @@
+export default {
+  "/news.html": {
+    title: "Новости",
+    newsCatalogHero: {
+      title: "новости",
+      logo: "/images/shared/logo-white.webp",
+    },
+    newsCatalogFilters: [
+      {
+        label: "все новости",
+        active: true,
+      },
+      {
+        label: "мероприятия",
+      },
+      {
+        label: "образование",
+      },
+      {
+        label: "карьера",
+      },
+      {
+        label: "машинное обучение",
+      },
+      {
+        label: "рейтинги",
+      },
+      {
+        label: "искусственный интеллект",
+      },
+      {
+        label: "веб-разработка",
+      },
+      {
+        label: "бренд",
+      },
+    ],
+    newsCatalogCards: [
+      {
+        href: "#",
+        image: "/images/news/news-catalog-1.webp",
+        alt: "Интерфейс калькулятора СберСтрахования",
+        imageModifier: "news-catalog-card__image--align-left",
+        title:
+          "Разработка единой дизайн-системы калькуляторов для «СберСтрахования»",
+        date: "15.03.2026",
+        tags: ["веб-разработка"],
+      },
+      {
+        href: "#",
+        image: "/images/news/news-catalog-2.webp",
+        alt: "Сертификат РБК",
+        title:
+          "Компания BIV вошла в число лучших работодателей России по версии РБК-2025",
+        date: "26.02.2026",
+        tags: ["мероприятия", "рейтинги"],
+      },
+      {
+        href: "#",
+        image: "/images/news/news-catalog-3.webp",
+        alt: "Торт с логотипом BIV",
+        title: "Что передает новый фирменный стиль компании BIV",
+        date: "15.02.2026",
+        tags: ["бренд", "рейтинги"],
+      },
+      {
+        href: "#",
+        image: "/images/news/news-1.webp",
+        alt: "Турнир по футболу",
+        title: "Турнир по футболу",
+        date: "13.01.2026",
+        tags: ["мероприятия"],
+      },
+      {
+        href: "#",
+        image: "/images/news/news-2.webp",
+        alt: "Конференция 2025",
+        title: "Конференция 2025",
+        date: "10.01.2026",
+        tags: ["мероприятия", "рейтинги"],
+      },
+      {
+        href: "#",
+        image: "/images/news/news-3.webp",
+        alt: "Разработчики взяли награду",
+        title: "Разработчики взяли награду",
+        date: "20.11.2025",
+        tags: ["веб-разработка", "рейтинги"],
+      },
+    ],
+  },
+};
